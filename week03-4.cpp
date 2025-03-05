eed7c07a4935c61b58c5b4d31d20e6cba67d28f0
ox0204 <goodzzoey@gmail.com>
2025-03-05 11:49:13 +0800

    week03

---
+++ week03-4.cpp
@@ -0,0 +1,15 @@
+///week03-4.cpp
+///M90H044 瘋狂程設不能打中文,會變亂碼
+#include <stdio.h>
+int main()
+{
+	int ans = 0; /// 答案加到這裡面
+	///迴圈前面 ans 是 0
+	int N; /// 有一個整數 N
+	scanf("%d" &N); /// 讀入 N
+	/// 人類的迴圈, 從1開始, <=
+	for(int i=1; i<=N; i++){
+		ans += i; /// 迴圈中間 修改 ans
+	}
+	printf("%d", ans);
+} /// 迴圈後面, 印出 ans
